3df9dd1112d2d3c32942a15f8d5291683f765ee1
Le Dinh Quyen <quyenld9699@gmail.com>
2025-11-03 15:50:01 +0700

    add special list token LFG

--- src/utils/specialIcons.ts
+++ src/utils/specialIcons.ts
@@ -5,25 +5,39 @@
  * For these tokens, we'll use {TOKEN}-lightmode and {TOKEN}-darkmode suffixes
  */
 export const specialTokens: string[] = [
-  "ALGO", "APT", "AR", "BTT", "BUSD",
-  "COMP", "FDUSD", "FLOKI", "HBAR", "HOT",
-  "HTX", "NFT", "ORAI", "ORAIX", "RAY",
-  "SOL", "TRUMP", "USDAI", "XLM", "XRP",
-  "ZEC", "ZRX"
+    "ALGO",
+    "APT",
+    "AR",
+    "BTT",
+    "BUSD",
+    "COMP",
+    "FDUSD",
+    "FLOKI",
+    "HBAR",
+    "HOT",
+    "HTX",
+    "LFG",
+    "NFT",
+    "ORAI",
+    "ORAIX",
+    "RAY",
+    "SOL",
+    "TRUMP",
+    "USDAI",
+    "XLM",
+    "XRP",
+    "ZEC",
+    "ZRX",
 ];
 
 /**
  * Wallets that have different images for light and dark mode
  * For these wallets, we'll use {wallet}-lightmode and {wallet}-darkmode suffixes
  */
-export const specialWallets: string[] = [
-  
-];
+export const specialWallets: string[] = [];
 
 /**
  * Systems that have different images for light and dark mode
  * For these systems, we'll use {system}-lightmode and {system}-darkmode suffixes
  */
-export const specialSystems: string[] = [
-  "Jito", "JustLendDAO", "Kamino", "Orchai"
-];
+export const specialSystems: string[] = ["Jito", "JustLendDAO", "Kamino", "Orchai"];
